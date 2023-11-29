c1d687763c35727a529816034a5b5f8613befe6a
MatteoArt <mattrim8@gmail.com>
2023-11-29 22:18:13 +0100

    init ricerca

--- src/pages/AppArticles.vue
+++ src/pages/AppArticles.vue
@@ -5,6 +5,7 @@ export default {
     data() {
         return {
             articlesList: articles,
+            searchForm: '',
         }
     },
     methods: {
@@ -13,6 +14,21 @@ export default {
         },
         getImagePath(img) {
             return new URL(`../assets/${img}`, import.meta.url).href;
+        },
+        searchTxt() {
+            const ricerca = this.searchForm;
+            console.log(ricerca);
+
+            //lista di tutti gli articoli dove effettuare la ricerca
+            const article = this.articlesList;
+
+            //array che conterrà tutte le stringhe nel json che soddisfano la ricerca
+            let paroleTrovate = [];
+
+            for (let i = 0; i < article.length; i++) {
+                console.log(article[i].title)
+                console.log(article[i].description)
+            }
         }
     }
 }
@@ -21,6 +37,19 @@ export default {
 
 <template>
     <h1 class="text-center">Articoli</h1>
+    <div>
+        <div class="m-auto d-flex justify-content-center" style="width: 250px;">
+            <input class="form-control border-end-0 rounded-end-0" 
+            type="text" placeholder="cerca..." v-model="searchForm" @keyup.enter="searchTxt">
+            <span class="my-button border border-start-0 d-flex align-items-center px-3 rounded-end-2"
+            @click="searchTxt">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </span>
+        </div>
+        <!-- !!! provvisorio !! -->
+        <span>Ricerca: {{ searchForm }}</span>
+    </div>
+
     <div class="container mx-auto mt-4 mb-3">
         <div class="row row-gap-3">
             <div class="col-md-6" v-for="article in articlesList">
@@ -34,7 +63,8 @@ export default {
                             {{ article.description }}
                         </p>
                         <div class="mt-auto">
-                            <router-link :to="{ name: 'single-article', params: { id: article.id} }" class="btn btn-outline-success rounded-pill">
+                            <router-link :to="{ name: 'single-article', params: { id: article.id } }"
+                                class="btn btn-outline-success rounded-pill">
                                 <i class="fa-solid fa-arrow-right" style="margin-right: 8px;"></i> Scopri di più
                             </router-link>
                         </div>
@@ -50,5 +80,13 @@ export default {
 .my-img-container {
     height: 272px;
     overflow: hidden;
+}
+input::placeholder {
+    text-align: center;
+    color: rgb(166, 166, 166);
+}
+.my-button {
+    color: rgb(166, 166, 166);
+    cursor: pointer;
 }
 </style>
